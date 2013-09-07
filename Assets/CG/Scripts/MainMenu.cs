a8cc007079285e14d1be9761cf707349afef4433
koemii <koemeterion@gmail.com>
2013-09-07 14:08:09 +0200

    Klassen Update

--- Assets/CG/Scripts/MainMenu.cs
+++ Assets/CG/Scripts/MainMenu.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
-
-
+    public string playerNameTextField = "Please enter your name";
+    public int choice = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -21,19 +21,48 @@ public class MainMenu : MonoBehaviour {
     void OnGUI()
     {
 
-        if (GUI.Button(new Rect(100,100,200,50), "Start"))
-        {
-
-            Application.LoadLevel(1);
-
-        }
-
-        if (GUI.Button(new Rect(100, 300, 200, 50), "Exit"))
-        {
-
-            Application.Quit();
         
+        switch(choice)
+        {
+            case 0:
+               if (GUI.Button(new Rect(100,100,200,50), "Start"))
+               {
+                      choice = 1;
+               }
+               if (GUI.Button(new Rect(100, 300, 200, 50), "Exit"))
+               {
+                      choice = 3;
+                }
+                break;
+            case 1:
+
+                
+                
+                playerNameTextField = GUI.TextArea(new Rect(100, 100, 175, 30), playerNameTextField, 23);
+                if (GUI.Button(new Rect(100, 150, 50, 50), "Start") && playerNameTextField != "Please enter your name")
+                {
+                    choice = 2;
+                }
+               
+                if (GUI.Button(new Rect(100, 220, 50, 50), "Back"))
+                {
+                    choice = 0;
+                }
+                break;
+            case 2:
+
+                Application.LoadLevel(1);
+
+                break;
+            case 3:
+
+                Application.Quit();
+                break;
         }
+
+        
+        
+
 
 
     }
