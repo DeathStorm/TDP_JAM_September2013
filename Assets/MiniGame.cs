da790ba760c4421478b3acb70ece67acd89e642c
ChristianT <ChristianT@onlyentertainment.de>
2013-09-07 11:40:00 +0200

    HIER

--- Assets/MiniGame.cs
+++ Assets/MiniGame.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using System.Collections;
 
-public class MiniGame : MonoBehaviour {
+public class MiniGame : MonoBehaviour
+{
 
     public Texture2D rahmen;
     public Texture2D balken;
+    public Texture2D ziel;
+    public Texture2D super;
 
     float curPos = 0;
     float curWidth = 10;
@@ -15,46 +18,80 @@ public class MiniGame : MonoBehaviour {
     float barHeight = 100;
     float ueberstand = 10;
 
+    float zielVon = 100.0f;
+    float zielBis = 350.0f;
+
+    float superVon = 150.0f;
+    float superBis = 200.0f;
+
+    public ERGEBNISSE ergebnis = ERGEBNISSE.KeinErgebnis;
+
+    public enum ERGEBNISSE { KeinErgebnis, Schlecht, Normal, Super };
+
     enum MovingDirection { Left, Right };
 
     MovingDirection curDirection = MovingDirection.Right;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         float maxBarWidth = barWidth - curWidth;
 
-        if (curDirection == MovingDirection.Right)
-        {
-            curPos = Mathf.Clamp(curPos+ Time.deltaTime * movingSpeed,curPos,maxBarWidth);
-        }
-        else if (curDirection == MovingDirection.Left)
-        {
-            curPos = Mathf.Clamp(curPos - Time.deltaTime * movingSpeed, 0, curPos);
-        }
+        if (Input.GetKeyDown(KeyCode.R)) { ergebnis = ERGEBNISSE.KeinErgebnis; }
 
-        if (curPos == maxBarWidth) curDirection = MovingDirection.Left;
-        else if (curPos == 0) curDirection = MovingDirection.Right;
-	
-	}
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (curPos >= superVon && curPos <= superBis)
+            {
+                ergebnis = ERGEBNISSE.Super; 
+            }
+            else if (curPos >= zielVon && curPos <= zielBis)
+            {
+                ergebnis = ERGEBNISSE.Normal;
+            }
+            else
+            {
+                ergebnis = ERGEBNISSE.Schlecht;
+            }
+        }
+        else if (ergebnis == ERGEBNISSE.KeinErgebnis)
+        {
+
+            if (curDirection == MovingDirection.Right)
+            {
+                curPos = Mathf.Clamp(curPos + Time.deltaTime * movingSpeed, curPos, maxBarWidth);
+            }
+            else if (curDirection == MovingDirection.Left)
+            {
+                curPos = Mathf.Clamp(curPos - Time.deltaTime * movingSpeed, 0, curPos);
+            }
+
+            if (curPos == maxBarWidth) curDirection = MovingDirection.Left;
+            else if (curPos == 0) curDirection = MovingDirection.Right;
+        }
+    }
 
 
 
     void OnGUI()
     {
         GUILayout.BeginArea(new Rect(Screen.width / 2 - barWidth / 2, Screen.height - barHeight * 2 - ueberstand, barWidth, barHeight + ueberstand * 2));
-        GUI.DrawTexture(new Rect(0,ueberstand,barWidth,barHeight),rahmen,ScaleMode.StretchToFill);
+        GUI.DrawTexture(new Rect(0, ueberstand, barWidth, barHeight), rahmen, ScaleMode.StretchToFill);
+        GUI.DrawTexture(new Rect(zielVon, ueberstand, zielBis-zielVon, barHeight), ziel, ScaleMode.StretchToFill);
+        GUI.DrawTexture(new Rect(superVon, ueberstand, superBis-superVon, barHeight), super, ScaleMode.StretchToFill);
 
+        GUI.DrawTexture(new Rect(curPos, 0, curWidth, barHeight + ueberstand * 2), balken, ScaleMode.StretchToFill);
         //GUI.Box(new Rect(0,ueberstand,barWidth,barHeight), "TEST");
 
-        GUI.Box(new Rect(curPos, 0, curWidth, barHeight + ueberstand * 2), "");
+        //GUI.Box(new Rect(curPos, 0, curWidth, barHeight + ueberstand * 2), "");
 
-           GUILayout.EndArea();
+        GUILayout.EndArea();
 
     }
 }
